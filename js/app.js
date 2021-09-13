f18593dae1aedaab8ef3ade8096412af56f7e2ef
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2021-09-13 12:57:27 +0600

    add

--- js/app.js
+++ js/app.js
@@ -30,14 +30,14 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-  updateTaxAndCharge();
+  updateTaxAndCharge();  
   document.getElementById("total-Products").innerText = count; 
-  updateTotal()    // update total solve
+   updateTotal() // update total solve
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element); // solved
   return converted;
 };
 
@@ -46,12 +46,15 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total).toFixed(2);
+  //document.getElementById(id).innerText = Math.round(total).toFixed(2);
+  document.getElementById(id).innerText = total.toFixed(2);
+  
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value).toFixed(2);
+  document.getElementById(id).innerText = parseFloat(value).toFixed(2);  //solved
+ 
 };
 
 // update delivery charge and total Tax
@@ -72,7 +75,7 @@ const updateTaxAndCharge = () => {
 };
 
 //grandTotal update function
-const updateTotal = (id, value) => {
+const updateTotal = () => {
  const grandTotal =getInputValue("price") + getInputValue("delivery-charge") + getInputValue("total-tax");
 document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
